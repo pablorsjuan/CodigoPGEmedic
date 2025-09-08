b22fd2f47faec292b22ed903f7ab9a0049fb8f96
Pablors <juanp.sucerquair@unac.edu.co>
2025-09-08 12:00:03 -0500

    segunda parte, la página principal ya tiene los botones como el mockup y otros cambios pequeños

---
+++ PUBLIC/JavaScript/Forgot_password.js
@@ -0,0 +1,5 @@
+document.getElementByClass("btn-send").addEventListener("click", function() {
+    // Mostrar los campos ocultos
+    document.getElementByClass("codigo").style.display = "flex";
+    document.getElementByClass("code-button").style.display = "flex";
+});
